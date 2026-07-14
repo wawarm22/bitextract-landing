d605eaec9cccfb033eaa20889d8bd1629056c300
watcharaphon <watcharaphon.wa@gmail.com>
2026-07-14 15:24:20 +0700

    feat: update Hero and Navbar components for improved text styling and logo dimensions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,13 +7,13 @@ export default function Navbar() {
       <div className="container-x flex h-[88px]">
         {/* logo */}
         <a href="https://www.think-bit.org/" className="flex items-center">
-          <img src="/assets/bit-extract.svg" alt="Think Bit Company Limited" className="h-8 max-w-[100px] sm:h-10 sm:max-w-[90px] md:h-[46px] md:max-w-[140px]" />
+          <img src="/assets/bit-extract.svg" alt="Think Bit Company Limited" className="h-8 max-w-[100px] sm:h-10 sm:max-w-[90px] md:h-[46px] md:max-w-[180px]" />
         </a>
 
         <div className="mx-3 my-auto h-7 w-px bg-black/100"/>        
 
         <a href="https://www.think-bit.org/" className="flex items-center">
-          <img src="/assets/thinkbit.svg" alt="Think Bit Company Limited" className="h-8 max-w-[100px] sm:h-10 sm:max-w-[90px] md:h-[46px] md:max-w-[140px]" />
+          <img src="/assets/thinkbit.svg" alt="Think Bit Company Limited" className="h-8 max-w-[100px] sm:h-10 sm:max-w-[90px] md:h-[46px] md:max-w-[180px]" />
         </a>
       </div>
     </header>
