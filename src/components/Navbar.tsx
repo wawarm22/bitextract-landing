16c061b80e130368be9f97aebd67fd9dba106905
watcharaphon <watcharaphon.wa@gmail.com>
2026-07-14 15:26:18 +0700

    feat: update logo dimensions for improved visual consistency in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,13 +7,13 @@ export default function Navbar() {
       <div className="container-x flex h-[88px]">
         {/* logo */}
         <a href="https://www.think-bit.org/" className="flex items-center">
-          <img src="/assets/bit-extract.svg" alt="Think Bit Company Limited" className="h-8 max-w-[100px] sm:h-10 sm:max-w-[90px] md:h-[46px] md:max-w-[180px]" />
+          <img src="/assets/bit-extract.svg" alt="Think Bit Company Limited" className="h-8 max-w-[120px] sm:h-10 sm:max-w-[130px] md:h-[46px] md:max-w-[170px]" />
         </a>
 
         <div className="mx-3 my-auto h-7 w-px bg-black/100"/>        
 
         <a href="https://www.think-bit.org/" className="flex items-center">
-          <img src="/assets/thinkbit.svg" alt="Think Bit Company Limited" className="h-8 max-w-[100px] sm:h-10 sm:max-w-[90px] md:h-[46px] md:max-w-[180px]" />
+          <img src="/assets/thinkbit.svg" alt="Think Bit Company Limited" className="h-8 max-w-[120px] sm:h-10 sm:max-w-[130px] md:h-[46px] md:max-w-[170px]" />
         </a>
       </div>
     </header>
